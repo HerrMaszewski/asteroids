8822bc0608a2a03c3d60895c7ad04ccc58e12228
HerrMaszewsk <tadmaszewski@gmail.com>
2024-09-15 23:44:29 +0200

    Rotate

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
+from pygame.examples.go_over_there import clock
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -13,8 +18,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen,(0, 0, 0))
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
